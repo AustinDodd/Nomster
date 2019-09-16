bf6399f4e3105a0697ae5af227efa217a5e309fc
Austin Dodd <austinsdodd@yahoo.com>
2019-09-16 20:04:36 +0000

    Fixed pagination to 10

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,8 +2,9 @@ class PlacesController < ApplicationController
 	before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
 	def index
-		@places = Place.order(:id).paginate(:page => params[:page], :per_page => 2)
+		@places = Place.order(:id).paginate(:page => params[:page], :per_page => 10)
 	end
+
 
 	def new
 		@place = Place.new
